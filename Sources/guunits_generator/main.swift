e6d523eba3f1ed389ada5a85a400a4c6e5e3b7b0
Callum McColl <callum.mccoll1@gmail.com>
2019-06-15 19:52:32 +1000

    Some more formatting

--- Sources/guunits_generator/main.swift
+++ Sources/guunits_generator/main.swift
@@ -14,21 +14,23 @@ let angleGenerator = AngleUnitsGenerator()
 
 do {
     try HeaderCreator().generate(
-    distanceGenerator: distanceGenerator,
-    timeGenerator: timeGenerator,
-    angleGenerator: angleGenerator).write(
-        to: URL(fileURLWithPath: "guunits.h", isDirectory: false),
-        atomically: false,
-        encoding: .utf8
-    )
+            distanceGenerator: distanceGenerator,
+            timeGenerator: timeGenerator,
+            angleGenerator: angleGenerator
+        ).write(
+            to: URL(fileURLWithPath: "guunits.h", isDirectory: false),
+            atomically: false,
+            encoding: .utf8
+        )
     try CFileCreator().generate(
-    distanceGenerator: distanceGenerator,
-    timeGenerator: timeGenerator,
-    angleGenerator: angleGenerator).write(
-        to: URL(fileURLWithPath: "guunits.c", isDirectory: false),
-        atomically: false,
-        encoding: .utf8
-    )
+            distanceGenerator: distanceGenerator,
+            timeGenerator: timeGenerator,
+            angleGenerator: angleGenerator
+        ).write(
+            to: URL(fileURLWithPath: "guunits.c", isDirectory: false),
+            atomically: false,
+            encoding: .utf8
+        )
 } catch (let e) {
     fatalError("\(e)")
 }
