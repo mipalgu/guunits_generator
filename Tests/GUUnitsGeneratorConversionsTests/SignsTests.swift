8220ad3a24f74064d0ebd065ac3072741529dfde
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-07-22 09:09:56 +1000

    Tested signs.

--- Tests/GUUnitsGeneratorConversionsTests/SignsTests.swift
+++ Tests/GUUnitsGeneratorConversionsTests/SignsTests.swift
@@ -5,18 +5,26 @@ final class SignsTests: XCTestCase {
 
     func testT() {
         XCTAssertEqual(Signs.t.rawValue, "t")
+        XCTAssertEqual(Signs.t.numericType, .int)
+        XCTAssertEqual(Signs.t.type, NumericTypes.int.rawValue)
     }
 
     func testU() {
         XCTAssertEqual(Signs.u.rawValue, "u")
+        XCTAssertEqual(Signs.u.numericType, .uint)
+        XCTAssertEqual(Signs.u.type, NumericTypes.uint.rawValue)
     }
 
     func testf() {
         XCTAssertEqual(Signs.f.rawValue, "f")
+        XCTAssertEqual(Signs.f.numericType, .float)
+        XCTAssertEqual(Signs.f.type, NumericTypes.float.rawValue)
     }
 
     func testD() {
         XCTAssertEqual(Signs.d.rawValue, "d")
+        XCTAssertEqual(Signs.d.numericType, .double)
+        XCTAssertEqual(Signs.d.type, NumericTypes.double.rawValue)
     }
 
 }
