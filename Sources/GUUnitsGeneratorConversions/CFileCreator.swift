b2814228a88706a01aefbe3552b19943ea772079
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-07-22 22:39:58 +1000

    Added comments.

--- Sources/GUUnitsGeneratorConversions/CFileCreator.swift
+++ Sources/GUUnitsGeneratorConversions/CFileCreator.swift
@@ -56,98 +56,136 @@
  *
  */
 
+/// Generates all of the C types and conversion functions.
 public struct CFileCreator {
-    
-    fileprivate var prefix: String {
-        return """
-            /*
-            * guunits.c
-            * guunits
-            *
-            * Created by Callum McColl on 05/06/2019.
-            * Copyright © 2019 Callum McColl. All rights reserved.
-            *
-            * Redistribution and use in source and binary forms, with or without
-            * modification, are permitted provided that the following conditions
-            * are met:
-            *
-            * 1. Redistributions of source code must retain the above copyright
-            *    notice, this list of conditions and the following disclaimer.
-            *
-            * 2. Redistributions in binary form must reproduce the above
-            *    copyright notice, this list of conditions and the following
-            *    disclaimer in the documentation and/or other materials
-            *    provided with the distribution.
-            *
-            * 3. All advertising materials mentioning features or use of this
-            *    software must display the following acknowledgement:
-            *
-            *        This product includes software developed by Callum McColl.
-            *
-            * 4. Neither the name of the author nor the names of contributors
-            *    may be used to endorse or promote products derived from this
-            *    software without specific prior written permission.
-            *
-            * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-            * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-            * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-            * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-            * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-            * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-            * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-            * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-            * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-            * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-            * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-            *
-            * -----------------------------------------------------------------------
-            * This program is free software; you can redistribute it and/or
-            * modify it under the above terms or under the terms of the GNU
-            * General Public License as published by the Free Software Foundation;
-            * either version 2 of the License, or (at your option) any later version.
-            *
-            * This program is distributed in the hope that it will be useful,
-            * but WITHOUT ANY WARRANTY; without even the implied warranty of
-            * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-            * GNU General Public License for more details.
-            *
-            * You should have received a copy of the GNU General Public License
-            * along with this program; if not, see http://www.gnu.org/licenses/
-            * or write to the Free Software Foundation, Inc., 51 Franklin Street,
-            * Fifth Floor, Boston, MA  02110-1301, USA.
-            *
-            */
-            \("\n")
-            #include "guunits.h"
-            \("")
-            #include <math.h>
-            \("")
-            #ifndef MAX
-            #define MAX(a, b) ((a) > (b) ? (a) : (b))
-            #endif
-            \("")
-            #ifndef MIN
-            #define MIN(a, b) ((a) < (b) ? (a) : (b))
-            #endif
-            \("")
-            #ifndef M_PI
-            #define M_PI 3.14159265358979323846
-            #endif
-            """
-    }
-    
-    var suffix: String {
-        return ""
+
+    /// The prefix which appears at the top of the file.
+    private var prefix: String {
+        """
+        /*
+        * guunits.c
+        * guunits
+        *
+        * Created by Callum McColl on 05/06/2019.
+        * Copyright © 2019 Callum McColl. All rights reserved.
+        *
+        * Redistribution and use in source and binary forms, with or without
+        * modification, are permitted provided that the following conditions
+        * are met:
+        *
+        * 1. Redistributions of source code must retain the above copyright
+        *    notice, this list of conditions and the following disclaimer.
+        *
+        * 2. Redistributions in binary form must reproduce the above
+        *    copyright notice, this list of conditions and the following
+        *    disclaimer in the documentation and/or other materials
+        *    provided with the distribution.
+        *
+        * 3. All advertising materials mentioning features or use of this
+        *    software must display the following acknowledgement:
+        *
+        *        This product includes software developed by Callum McColl.
+        *
+        * 4. Neither the name of the author nor the names of contributors
+        *    may be used to endorse or promote products derived from this
+        *    software without specific prior written permission.
+        *
+        * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+        * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+        * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+        * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+        * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+        * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+        * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+        * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+        * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+        * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+        * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+        *
+        * -----------------------------------------------------------------------
+        * This program is free software; you can redistribute it and/or
+        * modify it under the above terms or under the terms of the GNU
+        * General Public License as published by the Free Software Foundation;
+        * either version 2 of the License, or (at your option) any later version.
+        *
+        * This program is distributed in the hope that it will be useful,
+        * but WITHOUT ANY WARRANTY; without even the implied warranty of
+        * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        * GNU General Public License for more details.
+        *
+        * You should have received a copy of the GNU General Public License
+        * along with this program; if not, see http://www.gnu.org/licenses/
+        * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+        * Fifth Floor, Boston, MA  02110-1301, USA.
+        *
+        */
+        \("\n")
+        #include "guunits.h"
+        \("")
+        #include <math.h>
+        \("")
+        #ifndef MAX
+        #define MAX(a, b) ((a) > (b) ? (a) : (b))
+        #endif
+        \("")
+        #ifndef MIN
+        #define MIN(a, b) ((a) < (b) ? (a) : (b))
+        #endif
+        \("")
+        #ifndef M_PI
+        #define M_PI 3.14159265358979323846
+        #endif
+        """
     }
 
+    /// The suffix which appears at the end of the file.
+    var suffix: String {
+        ""
+    }
+
+    /// Default init.
     public init() {}
-    
-    public func generate(distanceGenerator: DistanceUnitsGenerator, timeGenerator: TimeUnitsGenerator, angleGenerator: AngleUnitsGenerator, imageGenerator: ImageUnitsGenerator, percentGenerator: PercentUnitGenerator) -> String {
-        let content = self.createContent(distanceGenerator: distanceGenerator, timeGenerator: timeGenerator, angleGenerator: angleGenerator, imageGenerator: imageGenerator, percentGenerator: percentGenerator)
+
+    /// Generate all of the guunits source code.
+    /// - Parameters:
+    ///   - distanceGenerator: The generator which creates distance functions.
+    ///   - timeGenerator: The generator which creates time functions.
+    ///   - angleGenerator: The generator which creates angle functions.
+    ///   - imageGenerator: The generator which creates image functions.
+    ///   - percentGenerator: The generator which creates percent functions.
+    /// - Returns: A string of all C functions for the supported guunits types.
+    public func generate(
+        distanceGenerator: DistanceUnitsGenerator,
+        timeGenerator: TimeUnitsGenerator,
+        angleGenerator: AngleUnitsGenerator,
+        imageGenerator: ImageUnitsGenerator,
+        percentGenerator: PercentUnitGenerator
+    ) -> String {
+        let content = self.createContent(
+            distanceGenerator: distanceGenerator,
+            timeGenerator: timeGenerator,
+            angleGenerator: angleGenerator,
+            imageGenerator: imageGenerator,
+            percentGenerator: percentGenerator
+        )
         return self.prefix + "\n\n" + content + "\n\n" + self.suffix
     }
-    
-    fileprivate func createContent(distanceGenerator: DistanceUnitsGenerator, timeGenerator: TimeUnitsGenerator, angleGenerator: AngleUnitsGenerator, imageGenerator: ImageUnitsGenerator, percentGenerator: PercentUnitGenerator) -> String {
+
+    /// Create the conversion functions for each unit type.
+    /// - Parameters:
+    ///   - distanceGenerator: The generator which creates distance functions.
+    ///   - timeGenerator: The generator which creates time functions.
+    ///   - angleGenerator: The generator which creates angle functions.
+    ///   - imageGenerator: The generator which creates image functions.
+    ///   - percentGenerator: The generator which creates percent functions.
+    /// - Returns: A string containing all of the conversion functions.
+    private func createContent(
+        distanceGenerator: DistanceUnitsGenerator,
+        timeGenerator: TimeUnitsGenerator,
+        angleGenerator: AngleUnitsGenerator,
+        imageGenerator: ImageUnitsGenerator,
+        percentGenerator: PercentUnitGenerator
+    ) -> String {
         guard
             let distances = distanceGenerator.generateImplementations(forUnits: DistanceUnits.allCases),
             let times = timeGenerator.generateImplementations(forUnits: TimeUnits.allCases),
@@ -163,5 +201,5 @@ public struct CFileCreator {
             + "\n\n// Image Conversion Functions\n\n" + images
             + "\n\n// Percent Conversion Functions\n\n" + percentages
     }
-    
+
 }
