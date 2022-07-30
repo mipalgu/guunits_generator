93a423047a48193d4860c9c9e291157a6ee92a8a
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-07-30 16:54:20 +1000

    Removed source files and created swift targets.

--- Tests/guunits/Package.swift
+++ Tests/guunits/Package.swift
@@ -3,13 +3,16 @@
 
 import PackageDescription
 
+/// Package description.
 let package = Package(
     name: "guunits",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible
+        // to other packages.
         .library(
             name: "guunits",
-            targets: ["guunits"]),
+            targets: ["guunits"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -17,12 +20,23 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages
+        // this package depends on.
         .target(
             name: "guunits",
-            dependencies: []),
+            dependencies: []
+        ),
+        .target(
+            name: "swift_GUUnits",
+            dependencies: ["guunits"]
+        ),
         .testTarget(
             name: "guunitsTests",
-            dependencies: ["guunits"]),
+            dependencies: ["guunits"]
+        ),
+        .testTarget(
+            name: "swift_GUUnitsTests",
+            dependencies: ["swift_GUUnits"]
+        )
     ]
 )
