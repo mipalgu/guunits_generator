bff030e5a755fdfce776b10db98b6d51b846cf48
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-07-30 21:30:41 +1000

    Added debug print.

--- Sources/GUUnitsGeneratorConversions/TestFileCreator.swift
+++ Sources/GUUnitsGeneratorConversions/TestFileCreator.swift
@@ -83,6 +83,7 @@ struct TestFileCreator<TestGeneratorType: TestGenerator> {
                 }
             }
         }
+        print("Created \(unitTests.count) Tests for \(Unit.category)!")
         let allTests = unitTests.joined(separator: "\n\n")
         return "\(imports)\nimport Foundation\nimport XCTest\n\nfinal class \(Unit.category)" +
             "Tests: XCTestCase {\n\n\(allTests)\n\n}\n"
